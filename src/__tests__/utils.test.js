ac5c2f80aa530de538b750cbbb86ef87e89e366d
Andrew <andrewstaveley14@gmail.com>
2023-12-14 23:03:54 -0500

    Completed Assignmnet

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,33 @@
-// Your tests here
+import { isPalindrome } from "../utils"
+
+describe("isPalindrome", () => {
+    it("returns true if a word is a palindrome", () => {
+        const word = "racecar"
+        const answer = isPalindrome(word)
+        expect(answer).toBe(true)
+    })
+
+    it("returns false is a word is not a palindrome", () => {
+        const word = "frankocean"
+        const answer = isPalindrome(word)
+        expect(answer).toBe(false)
+    })
+    
+    it("returns false for one letter inputs", () => {
+        const word = "a"
+        const answer = isPalindrome(word)
+        expect(answer).toBe(false)
+    })
+
+    it("is not case sensitive", () => {
+        const word = "TaCoCat"
+        const answer = isPalindrome(word)
+        expect(answer).toBe(true)
+    })
+
+    it("returns false for empty strings", () => {
+        const word = ""
+        const answer = isPalindrome(word)
+        expect(answer).toBe(false)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
-// Your code here
+export function isPalindrome(inputWord) {
+    if (inputWord.length <= 1) {
+        return false;
+    } else {
+        const wordArray = inputWord.toLowerCase().split("")
+        let arrayLength = wordArray.length / 2
+        let y = Math.floor(arrayLength)
+        const wordPartOne = wordArray.slice(0, y).join("")
+        const wordPartTwo = wordArray.slice(-y).reverse().join("")
+        return (wordPartOne === wordPartTwo)
+    }
+}
